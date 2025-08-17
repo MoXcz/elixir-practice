e960aec4b48404c80b818ca5228d36d5a5d331c9
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-08-16 18:40:43 -0600

    feat(topics): Expand on functions

--- topics/function.exs
+++ topics/function.exs
@@ -1,3 +1,27 @@
+"""
+Note that variables and functions ocupy different namespaces, so something like:
+
+  def add(a, b) do
+    a + b
+  end
+
+  def increment(num) do
+    add = 1
+    add(num, add)
+  end
+
+Works without problem, even though the identifier 'add' is being used both as
+a function and as a variable in the same scope.
+
+> This is a characteristic of LISP 2 languages
+
+This helps explain why anonymous functions use an extra dot '.', as this helps
+differentiate between a function, and a variable that has a function.
+
+> Because an anonymous function is essentially just binded to a variable, it
+  goes to the variable namespace
+"""
+
 age = IO.gets("What is your age? ")
 
 # anonymous functions are declared using 'fn'
@@ -10,7 +34,7 @@ eligibility = fn age ->
   end
 end
 
-# and then invoked using fn.(params)
+# and then invoked using fn.(params), with an added dot before the parameters
 
 defmodule Vote do
   # named functions
